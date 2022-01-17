861323e2e2938b63b033127ecf07fe4535acb83e
MateusKen <54600427+MateusKen@users.noreply.github.com>
2022-01-17 18:28:17 -0300

    Add files via upload

---
+++ voceeocara.py.py
@@ -0,0 +1,21 @@
+import discord
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('token')
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0,user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author ==  client.user:
+    return
+  if message.content.startswith('kawa'):
+    await message.channel.send('você é o cara!')
+
+client.run(TOKEN)
+
