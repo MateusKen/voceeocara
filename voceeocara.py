a8f7ec1c7b6c0ec8ee87c8e00d6ea8b401abaffa
MateusKen <54600427+MateusKen@users.noreply.github.com>
2022-02-17 21:45:39 -0300

    Update voceeocara.py

--- voceeocara.py
+++ voceeocara.py
@@ -1,6 +1,7 @@
 import discord
 import os
 from dotenv import load_dotenv
+from discord.ext.commands import Bot
 
 load_dotenv()
 TOKEN = os.getenv('token')
@@ -10,12 +11,17 @@ client = discord.Client()
 async def on_ready():
   print('We have logged in as {0,user}'.format(client))
 
+bot = Bot('!')
+@bot.command()
+async def test(ctx):
+  await ctx.send("This is a tts message", tts=True)
+
 @client.event
 async def on_message(message):
   if message.author ==  client.user:
     return
   if message.content.startswith('kawa'):
-    await message.channel.send('/tts <@378979413255258132>, você é o cara!', tts=True)
+    await message.channel.send('/tts <@378979413255258132>, você é o cara!')
 
 client.run(TOKEN)
 
